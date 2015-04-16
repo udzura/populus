42639766da8751d7663b8ee2179c86f59a21db2c
Uchio KONDO <udzura@pepabo.com>
2015-04-16 19:52:10 +0900

    Find from pool

--- lib/populus/watch_thread.rb
+++ lib/populus/watch_thread.rb
@@ -1,6 +1,8 @@
 require 'thread'
 require 'open3'
 
+require 'populus/pool'
+
 module Populus
   class WatchThread
     def self.consul_watch(*args)
@@ -9,8 +11,17 @@ module Populus
           Populus.consul_bin, 'watch', *args, '/bin/cat'
         )
         stdin.close
+        name = args[args.index('-name') + 1]
+        accepters = Populus::Pool.find_events_by_name(name)
         while l = stdout.gets
-          Populus.logger.info "accept JSON: %s" % l
+          Populus.logger.debug "accept JSON: %s" % l
+          accepters.each do |accepter|
+            begin
+              accepter.accept(l)
+            rescue => e
+              Populus.logger.warn "Error on event: %s, %s. Skip." % [e.class, e.message]
+            end
+          end
         end
       end
       return wait
