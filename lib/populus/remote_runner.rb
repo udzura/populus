52713098ad64333c52b6116dbda65c90a5f1a802
Uchio KONDO <udzura@pepabo.com>
2015-04-19 13:39:00 +0900

    Add create_file helper

--- lib/populus/remote_runner.rb
+++ lib/populus/remote_runner.rb
@@ -1,4 +1,7 @@
 require 'specinfra'
+require 'erb'
+require 'tempfile'
+require 'fileutils'
 
 module Populus
   class RemoteRunner
@@ -15,5 +18,17 @@ module Populus
       Populus.logger.debug("stderr:\n%s" % res.stderr)
       Populus.logger.info("Command exited: %d" % res.exit_status)
     end
+
+    def create_file(path, template="", context=nil)
+      file = Tempfile.new(".populus-tmp")
+      content = ERB.new(template).result(context || binding)
+      file.write content
+      file.close
+      @backend.send_file(file.path, path)
+      Populus.logger.info("Created Successfully: %s" % path)
+
+      FileUtils.rm_f(file.path)
+    end
+
   end
 end
